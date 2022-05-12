326b6ae5b2e70894af4aa664fac23f34d80ae9b4
staring-rita <tovmasanmargarita2@gmail.com>
2022-05-12 10:54:26 +0700

    d

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+from turtle import width
 import handtrakingmodule as htm
 import cv2
 import os
+
+folderPath = "Header"
+listHeader = os.listdir(folderPath)
+print(listHeader)
+
+cap = cv2.VideoCapture(0)
+width = 1920
+height = 1080
+cap.set (cv2.CAP_PROP_FRAME_WIDTH, width)
+cap.set (cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+cv2.namedWindow(W)
+while cap.isOpened():  # пока камера "работает"
+        success, image = cap.read()  # полчаем кадр с web-камеры (True/False, image)
+        if not success:  # если не удалось получить кадр
+            print("Не удалось получить изображение с web-камеры")
+            continue  # переход к ближайшему циклу (while)
+        
+        image = cv2.flip(image, 1)  # зеркальное отражение картинки
+        tector.findPosition(image, i, True)
+        
